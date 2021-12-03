a780d5a6639514a871b6dfa7975af95129124869
Alex-Nagy <nagyal3x@gmail.com>
2021-12-03 12:22:07 +0100

    flags

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function loadEvent() {
-    console.log('the page has loaded');
-    
+const renderCounties = (arr) => {
+  console.log(arr);
+  let returnHtml = "";
+  for (const country of arr) {
+    console.log(country.name.official);
+    returnHtml += `<div>${country.name.official}</div>`;
+  }
+  return returnHtml;
+};
+
+const renderFlags = (arr) => {
+    let returnHtml = ""
+    for (const flag of arr) {
+        returnHtml += `<img src="${flag.flags.png}">`
+    }
+    return returnHtml
+}
+
+async function loadEvent() {
+  console.log("the page has loaded");
+
+  const countryRes = await fetch("https://restcountries.com/v3.1/all");
+  const countryArr = await countryRes.json();
+
+  //   renderCounties(countryArr);
+  document
+    .getElementById("root")
+    .insertAdjacentHTML("beforeend", `<div id="countryNames"${renderCounties(countryArr)}`);
+  document
+    .getElementById("root")
+    .insertAdjacentHTML("beforeend", `<div id="countryFlags"${renderFlags(countryArr)}`);
 }
 
 window.addEventListener("load", loadEvent);
